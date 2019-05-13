cf3fa07b232c839f07920c1d825c803276e25122
Irsyad Rizaldi <irsyad.rizaldi97@gmail.com>
2019-05-14 03:21:56 +0700

    Order Service Unit Test

---
+++ src/test/java/com/foodstore/service/OrderServiceTests.java
@@ -0,0 +1,54 @@
+package com.foodstore.service;
+
+import com.foodstore.entity.Order;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"/spring-beans.xml", "/spring-mvc.xml", "/spring-shiro-web.xml"})
+@WebAppConfiguration
+@DirtiesContext
+public class OrderServiceTests {
+    @Autowired
+    private OrderService service;
+
+    @Test
+    public void test_createOrder_expectCreated() {
+        Order order = createSampleOrder();
+        service.createOrder(order);
+    }
+
+    @Test
+    public void test_deleteOrder_expectDeleted() {
+        Order order = createSampleOrder();
+        long createdOrderId = service.createOrder(order);
+        service.deleteOrder(createdOrderId);
+    }
+
+    @Test
+    public void test_findOneOrder_expectFound() {
+        Order order = createSampleOrder();
+        long createdOrderId = service.createOrder(order);
+        Order foundedOrder = service.findOne(createdOrderId);
+        Assert.assertNotNull(foundedOrder);
+    }
+
+    @Test
+    public void test_submitOrder_expectSubmitted() {
+        service.submitOrder("Submit Customer", "Submit Amount");
+    }
+
+    private Order createSampleOrder() {
+        Order order = new Order();
+        order.setId(0);
+        order.setCustomer("Sample Customer");
+        order.setAmount("Sample Amount");
+        return order;
+    }
+}
